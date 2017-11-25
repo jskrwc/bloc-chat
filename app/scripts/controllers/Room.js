bd29eff49b453dc944c1e52e9e07410b01f4a7ce
Jim Knopf <jsk4@mac.com>
2017-11-24 18:12:46 -0800

    Checkpoint Bloc-Chat 2 -- 1/2 completed

---
+++ app/scripts/controllers/Room.js
@@ -0,0 +1,17 @@
+// Room factory   bloc-chat-2
+
+(function() {
+  function Room($firebaseArray) {
+    var Room = {};
+    var ref = firebase.database().ref().child("rooms");
+    var rooms = $firebaseArray(ref);
+
+    Room.all = rooms;
+
+    return Room;
+  }
+
+  angular
+    .module('blocChat')
+    .factory ('Room', ['$firebaseArray', Room]);
+})();
