d0e2143c845bb7b8b4fbcb25ce769c7941875495
Jim Knopf <jsk4@mac.com>
2017-11-27 11:04:28 -0800

    Checkpoint 2 completed - controller added to display rooms

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,11 @@
 (function() {
-    function HomeCtrl() {
+    function HomeCtrl(Room) {
+//cp2 part 2
+      this.rooms = Room.all;
+
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', [HomeCtrl]);
+        .controller('HomeCtrl', ['Room', HomeCtrl]);
 })();
